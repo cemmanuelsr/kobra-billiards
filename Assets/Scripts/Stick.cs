1cc6cd3a2e8a96eafce418695f0f529aa39bb377
Diego Saragoza da Silva <diegosaragozaa2@gmail.com>
2022-11-02 15:11:48 -0300

    AI e rounds

--- Assets/Scripts/Stick.cs
+++ Assets/Scripts/Stick.cs
@@ -10,6 +10,7 @@ public class Stick : MonoBehaviour
     private SphereCollider ballCollider;
     private CapsuleCollider capsuleCollider;
     private GameObject targetHolder;
+    private int roundNumber;
     private float signal;
     private float playForce;
     private float angle;
@@ -20,6 +21,7 @@ public class Stick : MonoBehaviour
         capsuleCollider = gameObject.GetComponent<CapsuleCollider>();
         ballCollider = targetBall.GetComponent<SphereCollider>();
 
+        roundNumber = 0;
         signal = 1.0f;
         playForce = 0.0f;
         angle = 0.0f;
@@ -29,48 +31,71 @@ public class Stick : MonoBehaviour
         Physics.IgnoreLayerCollision(6, 0, true);
     }
 
-    void Update() {        
+    void Update() {
         if (!isGamePlaying) {
-            gameObject.GetComponent<Renderer>().enabled = true;
             Vector3 ballPosition = targetBall.transform.position;
-
-            if (Input.GetKey(KeyCode.UpArrow))
-                zoom += 0.1f * Time.deltaTime;
-            if (Input.GetKey(KeyCode.DownArrow))
-                zoom -= 0.1f * Time.deltaTime;
-            zoom = Mathf.Clamp(zoom, 0.5f, 1.5f);
-
             CameraFollow followScript = Camera.main.GetComponent<CameraFollow>();
-            followScript.setNewOffset(new Vector3(0.0f, zoom, 0.25f));
 
-            if (Input.GetKey(KeyCode.LeftArrow))
-                angle += 0.5f * Time.deltaTime;
-            if (Input.GetKey(KeyCode.RightArrow))
-                angle -= 0.5f * Time.deltaTime;
+            // Player's round
+            if (roundNumber % 2 == 0) {
+                gameObject.GetComponent<Renderer>().enabled = true;
 
-            // Lock stick at target ball
-            float radius = 1.15f * ballCollider.radius + playForce / 100.0f;
-            Vector3 newPosition = new Vector3(radius * Mathf.Sin(angle), 0.0f, radius * Mathf.Cos(angle)) + ballPosition;
-            transform.position = newPosition;
+                if (Input.GetKey(KeyCode.UpArrow))
+                    zoom += 0.1f * Time.deltaTime;
+                if (Input.GetKey(KeyCode.DownArrow))
+                    zoom -= 0.1f * Time.deltaTime;
+                zoom = Mathf.Clamp(zoom, 0.5f, 1.5f);
 
-            transform.LookAt(ballPosition);
-            
-            if (Input.GetKey(KeyCode.Space)) {
-                playForce += signal * 5.0f * Time.deltaTime;
-                playForce = Mathf.Clamp(playForce, 0.0f, 15.0f);
+                followScript.setNewOffset(new Vector3(0.0f, zoom, 0.25f));
 
-                if (playForce >= 14.9f) {
-                    signal = -1.0f;
-                } else if (playForce <= 0.1f) {
-                    signal = 1.0f;
+                if (Input.GetKey(KeyCode.LeftArrow))
+                    angle += 0.5f * Time.deltaTime;
+                if (Input.GetKey(KeyCode.RightArrow))
+                    angle -= 0.5f * Time.deltaTime;
+
+                // Lock stick at target ball
+                float radius = 1.15f * ballCollider.radius + playForce / 100.0f;
+                Vector3 newPosition = new Vector3(radius * Mathf.Sin(angle), 0.0f, radius * Mathf.Cos(angle)) + ballPosition;
+                transform.position = newPosition;
+
+                transform.LookAt(ballPosition);
+
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    playForce += signal * 5.0f * Time.deltaTime;
+                    playForce = Mathf.Clamp(playForce, 0.0f, 15.0f);
+
+                    if (playForce >= 14.9f)
+                    {
+                        signal = -1.0f;
+                    }
+                    else if (playForce <= 0.1f)
+                    {
+                        signal = 1.0f;
+                    }
+                }
+
+                // Make play
+                if (Input.GetKeyUp(KeyCode.Space))
+                {
+                    Rigidbody ballBody = targetBall.GetComponent<Rigidbody>();
+                    Vector3 forceDir = ballPosition - transform.position;
+                    ballBody.velocity = forceDir * playForce;
+                    dettachTargetBall();
+
+                    followScript.setNewTarget(GameObject.Find("Table Play Anchor"));
+                    followScript.setNewOffset(new Vector3(0.0f, 1.0f, 0.0f));
+
+                    isGamePlaying = true;
+                    playForce = 1.0f;
                 }
             }
-
-            // Make play
-            if (Input.GetKeyUp(KeyCode.Space)) {
+            // AI's round
+            else {
                 Rigidbody ballBody = targetBall.GetComponent<Rigidbody>();
                 Vector3 forceDir = ballPosition - transform.position;
-                ballBody.velocity = forceDir * playForce;
+                forceDir = Quaternion.Euler(0.0f, Random.Range(-45.0f, 45.0f), 0.0f) * forceDir;
+                ballBody.velocity = forceDir * Random.Range(7.5f, 15.0f);
                 dettachTargetBall();
 
                 followScript.setNewTarget(GameObject.Find("Table Play Anchor"));
@@ -89,7 +114,8 @@ public class Stick : MonoBehaviour
                 followScript.setNewTarget(GameObject.Find("Stick Anchor"));
                 followScript.setNewOffset(new Vector3(0.0f, zoom, 0.25f));
 
-                isGamePlaying = false;                
+                isGamePlaying = false;
+                roundNumber++;
             }
         }
     }
